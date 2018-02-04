81079f2a8544a6540947e3e9ffa64971a4173ea0
Geoffrey Card <gercunderscore4@gmail.com>
2018-02-04 17:27:31 -0500

    realized this file had the wrong contents, fixed that

--- read_encoded.m
+++ read_encoded.m
@@ -1,34 +1,29 @@
 %{
-	reads a txt file and calculates the char frequency and 
-	first order transition frequency
-	PARAMETERS:
-		book is the name of the file
-		ascii is a vertical array of chars to be counted
-	RETURNS:
-		freq_alpha is a vertical array matching the frequenies of chars in ascii
-		trans_ascii is a table of first order trans_ascii,
-			size len_ascii x len_ascii,
-			trans_ascii * previous = next
 %}
-function [freq_alpha trans_ascii] = read_a_book(book, ascii)
-
-	% open book
-	bookfile = fopen(book, 'r');
+function [freq_code trans_code textin] = read_encoded(filename, ascii)
 
 	len_ascii = length(ascii);
-	freq_alpha = zeros(len_ascii,1);
-	trans_ascii = zeros(len_ascii);
+
+	% open message file
+	bookfile = fopen(filename, 'r');
+
+	% initialize code
+	len_code = len_ascii;
+	code = zeros(1,len_code);
+	freq_code = zeros(1,len_code);
+	trans_code = zeros(len_ascii,len_code);
+
+	% initialize textin
+	textin = ' ';
+	len_textin = 0;
 
 	% for retrieving chars one at a time
 	onechar = '\0';
-
-	% number of characters in book
-	len_book = 0;
-
+	
 	% previous character index
 	prev = 0;
 
-	% fill out character frequency and trans_ascii
+	% find chars and check their frequency
 	while 1;
 		onechar = fscanf(bookfile, '%c', 1);
 
@@ -37,34 +32,31 @@ function [freq_alpha trans_ascii] = read_a_book(book, ascii)
 		end;
 
 		% incrememt character frequency
-		for ii=1:len_ascii;
-			if ascii(ii) == onechar;
-				% increment frequency
-				freq_alpha(ii) = freq_alpha(ii) + 1;
-			
-				% if there's a previous character
-				if prev ~= 0;
-					trans_ascii(prev,ii) = trans_ascii(prev,ii) + 1;
-				end;
-				
-				% current char becomes previous char
-				prev = ii;
+		ii = find(onechar == ascii, 1, 'first');
+        if ii;
+            % increment frequency
+            freq_code(ii) = freq_code(ii) + 1;
+        
+            % if there's a previous character
+            if prev ~= 0;
+                trans_code(prev,ii) = trans_code(prev,ii) + 1;
+            end;
+            
+            % current char becomes previous char
+            prev = ii;
 
-				% increment total number of chars
-				len_book = len_book + 1;	
-			end;
-		end;
+            % increment total number of chars
+            len_textin = len_textin + 1;	
+
+            % record
+            textin(len_textin) = onechar;
+        end;
 	end;
-	
+
 	% normalize
-	%freq_alpha = freq_alpha / len_book;
-	%for ii=1:size(trans_ascii,1)
-	%	some = sum(trans_ascii(ii,:));
-	%	if some ~= 0
-	%		trans_ascii(ii,:) = trans_ascii(ii,:) / some;
-	%	end
-	%end
+	%freq_code = freq_code / len_textin;
+	%trans_code = trans_code / len_textin; % no normanlized in current code
 	
-	% close the book
+	% close message file
 	fclose(bookfile);
 end
